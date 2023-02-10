4b5cf1416199c206f9048c7753c4e33d5c6ad0ca
James Saiban <jrs@Jamess-MacBook-Pro.local>
2023-02-09 19:43:03 -0500

    alpha22

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Bulkbeef from './pages/BulkBeef';
+import Bulkbeef from './pages/Bulkbeef';
 import CottegeCheese from './pages/CottegeCheese';
 import Home from './pages/Home';
 import Links from './pages/links';
